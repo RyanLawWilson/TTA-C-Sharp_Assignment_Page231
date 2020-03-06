93833129c06091e5a54a629210e2a5c28c6dc367
RyanLawWilson <59398576+RyanLawWilson@users.noreply.github.com>
2020-03-06 15:16:10 -0700

    Add project files.

---
+++ Page231/Dude.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Page231
+{
+    class Dude
+    {
+        // This pretty much allows you to put in optional parameters that are not compile-time constants.
+        // I you try to do this with an optional parameter, you get "Default parameter value for id" must be compile-time contant
+        //public Dude(string name, Guid id = Guid.NewGuid())
+        //{
+
+        //}
+        // If only the name is passed, use the referenced constructor but set id to Guid.NewGuid()
+        public Dude(string name) : this(name, Guid.NewGuid())
+        {
+            IsRandomID = true;
+        }
+        // Main constructor
+        public Dude(string name, Guid id)
+        {
+            Name = name;
+            ID = id;
+            IsRandomID = false;
+        }
+
+        public string Name { get; set; }
+        public Guid ID { get; set; }
+        public bool IsRandomID { get; set; }
+
+        public void Speak()
+        {
+            Console.WriteLine($"'YOOooooooo... My name is {Name}'");
+            System.Threading.Thread.Sleep(1000);
+
+            if (IsRandomID) Console.WriteLine($"\t'I was built with the chained constructor'");
+            else Console.WriteLine($"\t'I was NOT built with the chained constructor'");
+
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine($"\t'My ID is {ID}'");
+        }
+    }
+}
---
+++ Page231/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Page231
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("This is some text that cannot be changed: ");
+            const string TEXT = "This cannot be changed";
+            Console.WriteLine(TEXT);
+
+            System.Threading.Thread.Sleep(1000);
+
+            var randomMath = Math.PI * Math.Sqrt(Math.Abs(Math.Sin(-6.239464)));      // Whatever this output is will be put in this variable.
+            Console.WriteLine($"\nThis is a variable using var keyword.  It's value is: {randomMath:F2}");
+
+            System.Threading.Thread.Sleep(1000);
+
+            Dude dude1 = new Dude("Kyle");
+            Console.Write("Creating a dude....  Dude says: ");
+            dude1.Speak();
+
+            System.Threading.Thread.Sleep(2000);
+
+            Dude dude2 = new Dude("Bobby", new Guid("cbb33efa-b350-473f-9c2d-417ee8ff2f13"));
+            Console.Write("Creating another dude....  Dude says: ");
+            dude2.Speak();
+
+            System.Threading.Thread.Sleep(1000);
+
+            Console.ReadLine();
+        }
+    }
+}
